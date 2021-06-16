075292848ee943e44aaed61912d96e1a53167edd
deukio <deukioval@gmail.com>
2021-06-16 19:09:56 +0200

    revert cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -16,54 +16,49 @@ from tqdm import tqdm
 
 class Account:
     def __init__(self,tweet):
-        self.creation_date = (datetime.now()-datetime.strptime(tweet['user']['created_at'],"%a %b %d %H:%M:%S +0000 %Y")).total_seconds()
+        self.creation_date = (datetime.now()-datetime.strptime(tweet['user']['created_at'],"%a %b %d %H:%M:%S +0000 %Y")).total_seconds()/(60*60*24)
         self.verified = tweet['user']['verified']
         try:
             self.bio_length = len(tweet['user']['description'])
         except:
             self.bio_length = 0
         self.id = tweet['user']['id']
-            
+
         self.tweet_count = 1
         self.follow_count = tweet['user']['friends_count']
         self.followers_count = tweet['user']['followers_count']
         self.tweet_length = len(tweet['text'])
-        self.tweet_url = int("http" in tweet['text'])
-        self.retweet_by_tweet = int(tweet['retweeted'])
-        self.hashtag_by_tweet = int(tweet['text'].count('#'))
-        self.favorite_count = tweet['favorite_count']
-        self.retweet_count = tweet['retweet_count']
-        self.quote_count = tweet['quote_count']
         
+        self.url_by_tweet = tweet['text'].count("http")
+        self.hashtag_by_tweet = tweet['text'].count('#')
+        self.reference_by_tweet = tweet['text'].count("@")
+        #self.favorite_count = tweet['favorite_count']
+        #self.retweet_count = tweet['retweet_count']
+        #self.quote_count = tweet['quote_count']
+        #self.retweet_by_tweet = int(tweet['retweeted'])
+
     def update(self,tweet):
         self.tweet_count += 1
         self.follow_count += tweet['user']['friends_count']
         self.followers_count += tweet['user']['followers_count']
         self.tweet_length += len(tweet['text'])
-        self.retweet_by_tweet += int(tweet['retweeted'])
-        self.hashtag_by_tweet += int(tweet['text'].count('#'))
-        self.tweet_url += int("http" in tweet['text'])
-        self.favorite_count += tweet['favorite_count']
-        self.retweet_count += tweet['retweet_count']
-        self.quote_count += tweet['quote_count']
+        self.url_by_tweet += tweet['text'].count("http")
+        self.hashtag_by_tweet += tweet['text'].count('#')
+        self.reference_by_tweet = tweet['text'].count("@")
 
 
     def toCSV(self):
         self.follow_count /= self.tweet_count
         self.followers_count /= self.tweet_count
         self.tweet_length /= self.tweet_count
-        self.tweet_url /= self.tweet_count
-        self.favorite_count /= self.tweet_count
-        self.retweet_count /= self.tweet_count
-        self.quote_count /= self.tweet_count
-        self.retweet_by_tweet /= self.tweet_count
+        self.url_by_tweet /= self.tweet_count
+        self.reference_by_tweet /= self.tweet_count
         self.hashtag_by_tweet /= self.tweet_count
-        return list(map(int,[self.id,self.creation_date,self.bio_length,self.verified,
-                             self.follow_count,self.followers_count,self.tweet_length,
-                             self.tweet_url,self.tweet_count,self.favorite_count,
-                             self.retweet_count,self.quote_count,self.hashtag_by_tweet,
-                             self.retweet_by_tweet
-                             ]))
+        aggressiveness = (self.tweet_count+self.url_by_tweet)/self.creation_date
+        visibility = (self.hashtag_by_tweet*11.4+self.reference_by_tweet*11.6)/140
+        danger = self.url_by_tweet
+        #return self.id,self.follow_count,self.tweet_length,self.url_by_tweet,self.reference_by_tweet,self.hashtag_by_tweet
+        return self.id,aggressiveness,visibility,danger,self.follow_count
 
 def main(size=2285,batch=10,start=0):
     start_time = time.time()
@@ -92,9 +87,4 @@ def profiler(func):
     stats = pstats.Stats('{}.profile'.format(func))
     stats.strip_dirs().sort_stats('time').print_stats()
     
-profiler('main(size=10,batch=1)')
-
-# for file in os.listdir('data/cleaned'):
-#     os.remove('data/cleaned/{}'.format(file))
-
-#test VGuich
+profiler('main(size=100,batch=10)')
